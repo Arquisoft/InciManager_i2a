46be005cbb65ab91638716f646c754cc26c14228
Javier Díez García <javi_codema@hotmail.com>
2018-03-13 21:05:33 +0100

    Added Kafka Producer to the project.
    Note: I commented the previous starting main. We will have to see what to do with it.

---
+++ src/main/java/manager/Message.java
@@ -0,0 +1,20 @@
+package manager;
+
+/**
+ * Created by herminio on 27/2/17.
+ */
+public class Message {
+
+    private String message;
+
+    public Message() {}
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
