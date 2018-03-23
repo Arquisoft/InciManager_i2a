e257c5740fbf27eac4748d083643ed4be4d3e411
javicodema <javi_codema@hotmail.com>
2018-03-23 10:48:57 +0100

    Advanced in the approach to obtain the JSON from Incident

--- src/main/java/manager/IncidentController.java
+++ src/main/java/manager/IncidentController.java
@@ -1,16 +1,24 @@
 package manager;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mongodb.util.JSON;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 import manager.incidents.InciValidator;
 import manager.incidents.Incident;
+import manager.incidents.IncidentDTO;
 import manager.producers.KafkaProducer;
+import util.IncidentToJson;
 
 @Controller
 public class IncidentController {
@@ -19,20 +27,31 @@ public class IncidentController {
 	private KafkaProducer kafkaProducer;
 	@Autowired
 	private InciValidator inciValidator;
+	@Autowired
+	private IncidentToJson incidentJson;
+	@Autowired
+	private JsonGenerator jsonGen;
+	@Autowired
+	private SerializerProvider serial;
+	@Autowired
+	private JsonFactory jsonFactory;
 
 	@RequestMapping("/")
 	public String landing(Model model) {
-		model.addAttribute("message", new Message());
-		return "index";
+		model.addAttribute("incident", new IncidentDTO());
+		return "/incident/add";
 	}
-
+	
 	@RequestMapping("/send")
-	public String send(Model model, @ModelAttribute Message message) {
-
-		Incident incident = null;// Need to change this when receiving object
-		if (inciValidator.isEmergency(incident)) {
-			
-			kafkaProducer.send("exampleTopic", message.getMessage());
+	public String send(Model model, @ModelAttribute IncidentDTO incident) throws IOException {
+		Writer writer=new StringWriter();
+		Incident incidentFinal = incident.getIncident();
+		//incidentFinal.setAgent();
+		//pendientes las properties
+		jsonGen=jsonFactory.createJsonGenerator(writer); //no sé si funciona
+		if (inciValidator.isEmergency(incidentFinal)) {
+			incidentJson.serialize(incidentFinal, jsonGen, serial);
+			kafkaProducer.send("incident", writer.toString());
 		}
 		return "data";
 	}
